aa237fb4435bcb7acf14d5bb4e0c9d94c66794f5
Xe <me@christine.website>
2022-05-14 16:17:30 +0000

    initial commit
    Signed-off-by: Xe <me@christine.website>

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Internet of sh0rkz";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            deno
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+          ];
+        };
+      }
+    );
+}
