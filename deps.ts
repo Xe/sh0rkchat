52b5cde750f1861321d762e45ab3536515ef2c69
Xe <me@christine.website>
2022-05-14 17:28:33 +0000

    html templating
    Signed-off-by: Xe <me@christine.website>

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 import * as Drash from "https://deno.land/x/drash@v2.5.4/mod.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { html } from "https://deno.land/x/html_template@0.2.1/mod.ts";
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import * as Drash from "https://deno.land/x/drash@v2.5.4/mod.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { html } from "https://deno.land/x/html_template@0.2.1/mod.ts";
 
 const db = new DB(Deno.env.get("DATABASE_PATH"));
 db.query(`
@@ -10,8 +11,17 @@ CREATE TABLE IF NOT EXISTS messages
   );
 `);
 
-class HomeResource extends Drash.Resource {
-    public paths = ["/"];
+class Files extends Drash.Resource {
+  paths = ["/static/.*"];
+
+  public GET(request: Drash.Request, response: Drash.Response) {
+    const path = new URL(request.url).pathname;
+    return response.file(`.${path}`); // response.file("./favicon.ico")
+  }
+}
+
+class Chat extends Drash.Resource {
+    public paths = ["/api/chat"];
 
     public POST(request: Drash.Request, response: Drash.Response): void {
         const message = request.bodyParam<string>("message");
@@ -36,13 +46,45 @@ class HomeResource extends Drash.Resource {
     }
 }
 
+class Index extends Drash.Resource {
+    public paths = ["/"];
 
+    public GET(request: Drash.Request, response: Drash.Response): void {
+        return html`
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <link rel="stylesheet" href="/static/xess.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body id="top">
+        <main>
+            <h1>Page title</h1>
+
+            <p>Hello! this is an example page using <a href="https://github.com/Xe/Xess">Xess</a></p>
+            
+            <blockquote>When in doubt, create another framework</blockquote>
+            
+            <ul><li>Unknown</li></ul>
+            
+            <p>Select this text to see what happens!</p>
+
+            <footer>
+                <p>CSS available under the MIT license.</p>
+            </footer>
+        </main>
+    </body>
+</html>
+`
+    }
+}
 
 const server = new Drash.Server({
     hostname: "0.0.0.0",
     port: 8080,
     protocol: "http",
-    resources: [HomeResource],
+    resources: [Chat, Files, Index],
 });
 
 server.run();
